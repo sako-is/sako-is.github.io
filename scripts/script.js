433bc3c1956f068aef2bf21bcafb27627dbab446
sako-is <isazadasaid@gmail.com>
2022-12-16 21:17:49 -0500

    init

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function english() {
+    document.getElementById("name").innerHTML = "Said Isazada"
+}
+
+function azeri() {
+    document.getElementById("name").innerHTML = "Səid İsazadə"
+}
+
+function russian() {
+    document.getElementById("name").innerHTML = "Саид Исазаде"
+}
+
+function turkish() {
+    document.getElementById("name").innerHTML = "Said İsazade"
+}
