b74cd24e641d15e3fd6632cc59765301eedcea03
sako-is <isazadasaid@gmail.com>
2023-11-28 07:59:43 -0500

    julian

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,19 @@
 function english() {
     document.getElementById("name").innerHTML = "Said Isazada"
+    document.getElementById("projects").innerHTML = "portfolio"
 }
 
 function azeri() {
-    document.getElementById("name").innerHTML = "Səid İsazadə"
+    document.getElementById("name").innerHTML = "Säid İsazadä"
+    document.getElementById("projects").innerHTML = "portfolio"
 }
 
 function russian() {
     document.getElementById("name").innerHTML = "Саид Исазаде"
+    document.getElementById("projects").innerHTML = "портфолио"
 }
 
 function turkish() {
-    document.getElementById("name").innerHTML = "Said İsazade"
+    document.getElementById("name").innerHTML = "Sait İsazade"
+    document.getElementById("projects").innerHTML = "portfolio"
 }
